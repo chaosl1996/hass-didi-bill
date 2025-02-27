e1f3037608f3f1db0f003cba1b373267cc2c9825
chaosl1996 <76881956+chaosl1996@users.noreply.github.com>
2025-02-27 15:47:57 +0800

    Update __init__.py

--- custom_components/didi_bill/__init__.py
+++ custom_components/didi_bill/__init__.py
@@ -1,19 +1,20 @@
-from homeassistant.core import HomeAssistant
+import logging
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.helpers.typing import ConfigType
-
+from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 
-async def async_setup(hass: HomeAssistant, config: ConfigType):
-    """加载 Home Assistant 配置（不再使用 `configuration.yaml`）"""
-    return True
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """从配置入口（config flow）加载组件"""
-    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    """设置入口"""
+    hass.data.setdefault(DOMAIN, {})
+
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    )
 
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """卸载实体"""
-    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    """卸载入口"""
+    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
