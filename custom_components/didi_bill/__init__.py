2fe02d25d1cec529b8c996dead09f4299d4717bf
chaosl1996 <76881956+chaosl1996@users.noreply.github.com>
2025-02-27 14:12:27 +0800

    Update __init__.py

--- custom_components/didi_bill/__init__.py
+++ custom_components/didi_bill/__init__.py
@@ -10,7 +10,8 @@ async def async_setup(hass: HomeAssistant, config: ConfigType):
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """从配置入口（config flow）加载组件"""
-    hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, "sensor"))
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
